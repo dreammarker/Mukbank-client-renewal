edd7110a751d2193775eaded7a6a692f798db3d7
YunJu Lee <ollabu3@gmail.com>
2020-07-28 23:50:32 +0900

    Feat: Drawer sidebar component -DrawerContentㄴLikeList,Signin,SignUp

---
+++ Screen/Home/DrawerContent/DrawerContent.tsx
@@ -0,0 +1,12 @@
+import React, {ReactElement} from 'react';
+import {DrawerItem, createDrawerNavigator} from '@react-navigation/drawer';
+import {Drawer} from 'react-native-paper';
+
+import SigninScreen from './Signin/SigninScreen';
+import SignUpScreen from './SignUp/SignUpScreen';
+import LikeListScreen from './LikeList/LikeListScreen';
+
+const DrawerContent = () => {
+  return <></>;
+};
+export default DrawerContent;
