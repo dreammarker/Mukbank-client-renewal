38f745f98b6e1bca033b0197f57abd29e30be2bd
YunJu Lee <ollabu3@gmail.com>
2020-08-27 00:40:54 +0900

    FilterChip선택 시 선택된 Chip - 선택되어 추가된 select(array)에 있는 name들 - Chip 선택 시 선택된 Chip name이 select에서 splice되어 제거 - toggleChips: 제거된 Chip name(canceledChip)과 ChipListData.name이 같다면 ChipData.isSelect = false되어 선택유무  취소

--- Screen/Main/Screen/Search/Components/SelectFilter/SelectFilter.tsx
+++ Screen/Main/Screen/Search/Components/SelectFilter/SelectFilter.tsx
@@ -1,26 +1,53 @@
-import React, {useState} from 'react';
-import {Chip, Button} from 'react-native-paper';
+/* eslint-disable react-hooks/exhaustive-deps */
+import React, {useState, useEffect} from 'react';
+import {Button} from 'react-native-paper';
 import {View, ScrollView} from 'react-native';
 
 import styles from './SelectFilterStyles';
 import ChipList from './Components/ChipList';
+import SelectedChip from './Components/SelectedChip';
 import {ChipListData} from './Components/ChipListData';
 
+interface ChipData {
+  name: string;
+  isSelected: boolean;
+}
+
 function SelectFilter() {
-  const [chipListData] = useState<Array<object>>(ChipListData);
-  const [select, setSelect] = useState<string>('');
-  console.log(select);
+  const [chipListData] = useState<ChipData[]>(ChipListData); // 기존 선택사항 chip들
+  const [select, setSelect] = useState<string[]>([]); // 선택된 chip의 name들
+  const [canceledChip, setCanceledChip] = useState<string>(''); // SelectedChip에서 취소된 chip의 name 표시
+
+  useEffect(() => {
+    // cancelChip과 chipListData[i].name이 같다면 chipListData[i].isSelected = false
+    const toggleChips = () => {
+      chipListData.forEach((curr: ChipData) => {
+        if (canceledChip === curr.name) {
+          curr.isSelected = false;
+        }
+      });
+    };
+    toggleChips();
+  }, [canceledChip]);
 
   return (
     <View>
       <View style={styles.selectedChipView as any}>
         <ScrollView horizontal={true}>
-          <Chip mode="outlined">한식</Chip>
+          {select.map((list: string) => (
+            <SelectedChip
+              key={JSON.stringify(list)}
+              list={list}
+              select={select}
+              setSelect={setSelect}
+              setCanceledChip={setCanceledChip}
+            />
+          ))}
         </ScrollView>
         <Button>검색</Button>
       </View>
       <View style={styles.selectedChipView as any}>
-        {chipListData.map((list: object) => (
+        {chipListData.map((list: ChipData) => (
           <View key={JSON.stringify(list)}>
             <ChipList list={list} select={select} setSelect={setSelect} />
           </View>
