bc190a73e9c3f22aac13d14ea8461c4c19c82772
YunJu Lee <ollabu3@gmail.com>
2020-08-27 14:43:49 +0900

    search, filtering text 서버 전송 함수 작성 - Chip 선택 후 검색 버튼 누를 시 string화 된 text 전송 (['한식','일식'  ==> '한식, 일식'), 만약 chip선택 없이 검색 버튼 누를 시 '필터를 선택 해 주세요' toast event 발생 - 만약 searchbar에서 전송하는 text없이 keyboard 검색 누를 시 '검색어를 입력해 주세요' toast event 발생

--- Screen/Main/Screen/Search/Components/SelectFilter/SelectFilter.tsx
+++ Screen/Main/Screen/Search/Components/SelectFilter/SelectFilter.tsx
@@ -1,22 +1,52 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 import React, {useState, useEffect} from 'react';
+import axios from 'axios';
 import {Button} from 'react-native-paper';
-import {View, ScrollView} from 'react-native';
+import {View, ScrollView, ToastAndroid} from 'react-native';
+import {StackNavigationProp} from '@react-navigation/stack';
 
 import styles from './SelectFilterStyles';
 import ChipList from './Components/ChipList';
 import SelectedChip from './Components/SelectedChip';
 import {ChipListData} from './Components/ChipListData';
 
+type NavigationProp = StackNavigationProp<MainStackNaviParamList>;
+
 interface ChipData {
   name: string;
   isSelected: boolean;
 }
 
-function SelectFilter() {
+interface SelectFilterProps {
+  navigation: NavigationProp;
+  location: {latitude: number; longitude: number};
+}
+
+function SelectFilter({location, navigation}: SelectFilterProps) {
   const [chipListData] = useState<ChipData[]>(ChipListData); // 기존 선택사항 chip들
   const [select, setSelect] = useState<string[]>([]); // 선택된 chip의 name들
   const [canceledChip, setCanceledChip] = useState<string>(''); // SelectedChip에서 취소된 chip의 name 표시
+
+  const sendFilterText = () => {
+    if (select.length === 0) {
+      // 필터 선택 한 것 없을 때
+      ToastAndroid.showWithGravity(
+        '필터를 선택 해 주세요.',
+        ToastAndroid.SHORT,
+        ToastAndroid.CENTER,
+      );
+    } else {
+      const filterText: string = select.join(', ');
+      axios
+        .post('http://192.168.0.4:5001/restaurant/selectFilter', {
+          latitude: location.latitude, // 37.570652,
+          longitude: location.longitude, // 127.007307,
+          filterText: filterText,
+        })
+        .catch((error) => console.log(error));
+      navigation.navigate('SearchList');
+    }
+  };
 
   useEffect(() => {
     // cancelChip과 chipListData[i].name이 같다면 chipListData[i].isSelected = false
@@ -44,7 +74,7 @@ function SelectFilter() {
             />
           ))}
         </ScrollView>
-        <Button>검색</Button>
+        <Button onPress={() => sendFilterText()}>검색</Button>
       </View>
       <View style={styles.selectedChipView as any}>
         {chipListData.map((list: ChipData) => (
