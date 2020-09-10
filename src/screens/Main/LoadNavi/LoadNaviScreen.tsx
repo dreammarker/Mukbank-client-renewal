1b2cf07e81653a4751dfcbb6f2c87c332ac72f91
YunJu Lee <ollabu3@gmail.com>
2020-09-11 01:34:39 +0900

    LoadNaviScreen MapView 랜더링 - LoadNaviScreen에서 유저의 location이 재설정 되어야 하니까 재 랜더링이 됐는지 check하기 위해 check라는 state를 이용하여 check가 true일 시 지도를 랜더링

--- src/screens/Main/LoadNavi/LoadNaviScreen.tsx
+++ src/screens/Main/LoadNavi/LoadNaviScreen.tsx
@@ -1,7 +1,46 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import {View} from 'react-native';
+import MapView from 'react-native-maps';
 
-function LoadNaviScreen() {
-  return <></>;
+interface LoadNaviProps {
+  GetCurrentLocation: any;
+  location: {latitude: number; longitude: number};
 }
+
+function LoadNaviScreen({GetCurrentLocation, location}: LoadNaviProps) {
+  const [check, setCheck] = useState<boolean>(false);
+
+  useEffect(() => {
+    GetCurrentLocation();
+    setCheck(true);
+  }, []);
+  return (
+    <View
+      style={{
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+        backgroundColor: '#F5FCFF',
+      }}>
+      {check ? (
+        <MapView
+          style={{left: 0, right: 0, top: 0, bottom: 0, position: 'absolute'}}
+          initialRegion={{
+            latitude: location.latitude,
+            longitude: location.longitude,
+            latitudeDelta: 0.005,
+            longitudeDelta: 0.005,
+          }}
+          showsUserLocation={true}
+          // showsMyLocationButton={true}
+          followsUserLocation={true}
+        />
+      ) : (
+        <></>
+      )}
+    </View>
+  );
+}
+//
 
 export default LoadNaviScreen;
