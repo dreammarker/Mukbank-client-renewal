60b2a870881d380840a7e41767cb4b20cc928427
YunJu Lee <ollabu3@gmail.com>
2020-09-23 01:22:36 +0900

    TextInput password 입력 체크 - passwordValidator: password.value가 특수문자,영대소문자,숫자포함 8~15 이내일 시false(양식에 맞음) 아님 true(안맞음) (false 일 시 HeleperText 띄움) - checkPassword: 비밀번호 다시한번 체크 - SignUpPressed: 회원가입 버튼 누를 시 passWordError가 true(양식에 맞지 않을 시) || checkPassword가 true일 시 password.error = true, chkPwd = true 가 되고, TextInput의 props인 error가 true가 되면서 에러표시

--- src/screens/Main/SignUp/SignUpScreen.tsx
+++ src/screens/Main/SignUp/SignUpScreen.tsx
@@ -15,17 +15,35 @@ interface Props {}
 
 function SignUpScreen({navigation}) {
   const [id, setId] = useState<checkText>({value: '', error: ''});
+  const [password, setPassword] = useState<checkText>({value: '', error: ''});
+  const [chkPwd, setChkPwd] = useState<checkText>({
+    value: '',
+    error: '',
+  });
 
   const idValidator = (idVal: string) => {
     var idReg = /^[a-zA-Z](?=.{0,28}[0-9])[0-9a-zA-Z]{5,8}$/; // 영숫자포함 5~8 정규표현식
     return !idReg.test(idVal);
   };
 
+  const passwordValidator = (passwordVal: string) => {
+    var passwordReg = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[$@$!%*?&])[A-Za-z\d$@$!%*?&]{8,15}$/; // 특수문자,영대소문자,숫자포함 8~15 정규표현식
+    return !passwordReg.test(passwordVal);
+  };
+
+  const checkPassword = (chkPasswordVal: string) => {
+    return !(chkPasswordVal === password.value);
+  };
+  console.log(chkPwd.error);
   const SignUpPressed = () => {
     const idError = idValidator(id.value);
-
-    if (idError) {
+    const passwordError = passwordValidator(password.value);
+    const chkPwdError = checkPassword(chkPwd.value);
+    if (idError || passwordError || chkPwdError) {
       setId({...id, error: idError});
+      setPassword({...password, error: passwordError});
+      setChkPwd({...chkPwd, error: chkPwdError});
+      return;
     } else {
       console.log('통과');
     }
@@ -34,7 +52,7 @@ function SignUpScreen({navigation}) {
   return (
     <View style={styles.background}>
       <KeyboardAvoidingView style={styles.container} behavior="padding">
-        <Text style={styles.header}>회원가입</Text>
+        <Text style={styles.header}>SignUp</Text>
         <View style={styles.inputContainer}>
           <TextInput
             label="아이디"
@@ -46,9 +64,39 @@ function SignUpScreen({navigation}) {
           />
           <HelperText type="error" visible={idValidator(id.value)}>
             {/* visible이 false면 에러 */}
-            아이디는 영숫자포함 5~8 이내 입니다.
+            영어, 숫자포함 5~8 이내
           </HelperText>
         </View>
+        <View style={styles.inputContainer}>
+          <TextInput
+            label="비밀번호"
+            mode={'outlined'}
+            value={password.value}
+            returnKeyType="done"
+            onChangeText={(text) => setPassword({value: text, error: ''})}
+            error={!!password.error}
+            secureTextEntry
+          />
+          <HelperText type="error" visible={passwordValidator(password.value)}>
+            특수문자, 영대소문자, 숫자포함 8~15 이내
+          </HelperText>
+        </View>
+
+        <View style={styles.inputContainer}>
+          <TextInput
+            label="비밀번호확인"
+            mode={'outlined'}
+            value={chkPwd.value}
+            returnKeyType="done"
+            onChangeText={(text) => setChkPwd({value: text, error: ''})}
+            error={!!chkPwd.error}
+            secureTextEntry
+          />
+          <HelperText type="error" visible={checkPassword(chkPwd.value)}>
+            일치하지 않습니다.
+          </HelperText>
+        </View>
+
         <Btn mode="contained" onPress={() => SignUpPressed()}>
           회원가입
         </Btn>
@@ -70,7 +118,6 @@ const styles = StyleSheet.create({
     padding: 20,
     width: '100%',
     maxWidth: 340,
-    marginTop: 100,
     alignSelf: 'center',
     alignItems: 'center',
   },
@@ -81,7 +128,7 @@ const styles = StyleSheet.create({
   },
   inputContainer: {
     width: '100%',
-    marginVertical: 12,
+    marginVertical: 6,
   },
   forgotPassword: {
     width: '100%',
