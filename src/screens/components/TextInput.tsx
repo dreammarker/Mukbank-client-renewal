9e39df1423b421705b1474f6f8f1bf312bb18367
YunJu Lee <ollabu3@gmail.com>
2020-09-23 13:58:28 +0900

    TextInput Component props 생성

---
+++ src/screens/components/TextInput.tsx
@@ -0,0 +1,25 @@
+import React, {memo} from 'react';
+import {View, StyleSheet} from 'react-native';
+import {TextInput as Input, HelperText} from 'react-native-paper';
+
+type Props = React.ComponentProps<typeof Input> & {errorText?: string} & {
+  visible: boolean;
+};
+
+const TextInput = ({visible, errorText, ...props}: Props) => (
+  <View style={styles.container}>
+    <Input underlineColor="transparent" mode="outlined" {...props} />
+    <HelperText type="error" visible={visible}>
+      {errorText}
+    </HelperText>
+  </View>
+);
+
+const styles = StyleSheet.create({
+  container: {
+    width: '100%',
+    marginVertical: 12,
+  },
+});
+
+export default memo(TextInput);
